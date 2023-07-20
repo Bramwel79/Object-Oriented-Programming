6d9380565a02885e20a7c784f054469a49a60dc1
Bramwel79 <140046556+Bramwel79@users.noreply.github.com>
2023-07-20 11:55:38 +0300

    Add files via upload

---
+++ ComputeWeeklySalary.cs/ComputeWeeklySalary.cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComputeWeeklySalary.cs
+{
+    class Program
+    {
+        const int WEEKS_IN_A_YEAR = 52;
+        static void Main(string[] args)
+        {
+            CalculateWeeklySalary(120000);
+            CalculateWeeklySalary(450000.55);
+            Console.ReadLine();
+        }
+        static void CalculateWeeklySalary(int annualSalary)
+        {
+            int weeklySalary = annualSalary / WEEKS_IN_A_YEAR;
+            Console.WriteLine("The weekly salary is {0}", weeklySalary.ToString ("c"));
+        }
+        static void CalculateWeeklySalary(double annualSalary)
+        {
+            double weeklySalary = annualSalary / WEEKS_IN_A_YEAR;
+            Console.WriteLine("The weekly salary is {0}", weeklySalary.ToString ("c"));
+        }
+    }
+}
